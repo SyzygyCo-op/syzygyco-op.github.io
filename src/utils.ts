87b2daa7bd48a1f92c18135156bc6c1105108822
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-03-30 19:38:03 -0700

    comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 
+/** Only created to support the SimpleTix widget presentation hack */
 export const rowClassName = "w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4 content-center";
+
+/** Only created to support the SimpleTix widget presentation hack */
 export const cardClassName = "border border-gray-200 flex-shrink bg-yellow-50";
