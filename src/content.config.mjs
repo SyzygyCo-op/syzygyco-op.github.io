da5df6c8573ea0caff65d8ed642f787232178f7a
Quentin Topper <quentopp@gmail.com>
2025-07-23 02:27:13 -0700

    Add custom Google sheets integration

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 
 // 2. Import loader(s)
 import { glob, file, folder } from 'astro/loaders';
+import { remoteCSVLoader } from '../loaders/remote_csv_loader'
 
 // 3. Define your collection(s)
 const members = defineCollection({
@@ -36,6 +37,32 @@ const events = defineCollection({
     }),
 });
 
+const specialEvents = defineCollection({
+    loader: remoteCSVLoader({
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9lwK4u_pUe90Ff5o-2OpGSAjMv2gN-CQF1vTBbwfZgKFLYMuHhW8rjp4fsyhhL6R9ANYalYBQRjmh/pub?gid=548759105&single=true&output=csv',
+    }),
+    schema: () => z.object({
+        'Name': z.optional(z.string()),
+        'Description': z.optional(z.string()),
+        'Date': z.optional(z.string()),
+        'Image URL': z.optional(z.string()),
+        'Image Description': z.optional(z.string()),
+    })
+})
+
+const recurringEvents = defineCollection({
+    loader: remoteCSVLoader({
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9lwK4u_pUe90Ff5o-2OpGSAjMv2gN-CQF1vTBbwfZgKFLYMuHhW8rjp4fsyhhL6R9ANYalYBQRjmh/pub?gid=0&single=true&output=csv'
+    }),
+    schema: () => z.object({
+        'Name': z.optional(z.string()),
+        'Description': z.optional(z.string()),
+        'Date': z.optional(z.string()),
+        'Image URL': z.optional(z.string()),
+        'Image Description': z.optional(z.string()),
+    })
+})
+
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { members, events, news };
+export const collections = { members, events, news, specialEvents, recurringEvents };
 
